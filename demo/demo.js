9659bce10f9a295e56cee4fa2148579e17516cab
Pranav Jeevan <pjofficial2000@gmail.com>
2022-03-29 23:44:27 +0530

    js

--- demo/demo.js
+++ demo/demo.js
@@ -1,3 +1,19 @@
+let x = 3,y = 4,z;
+z = x +y ;
+
+const boy = {
+    name:"Pranav",
+    age: 21
+}
+
 function msg(){
     document.getElementById("p3").innerHTML="This is the new changed para!"
+}
+
+function add(){
+    window.alert(z)
+}
+
+function disp(){
+    window.alert("Name ="+boy.name+" "+"age= "+boy.age)
 }
